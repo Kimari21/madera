26f3a3402e8ad463b6f87032c1d5773d90365f3b
Julien Benoit <benoitjulien21560@gmail.com>
2017-01-30 17:25:43 +0100

    faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -5,5 +5,25 @@
 @endsection
 
 @section('contenu')
-    <p>Questions</p>
+    <div class='col-md-4 col-md-offset-4'>
+    	<table class="table table-bordered">
+    		<tr>
+    			<td>
+    				<h1>Voici les questions fréquemment posées</h1>
+
+
+    				<h2>Qu'est-ce qu'une maison modulaire?</h2>
+    				<p>Une maison modulaire est une maison de base pouvant être modifiée aux grés des clients par des modules.
+    					Ces modules permettent de modifier certaines parties de la maison comme par exemple le toit, les murs ou le sol
+    					avec des matériaux et dimensions différents</p>
+
+    				<h3>Qu'est ce qu'une maison modulaire apporte de plus?</h3>
+    				<p>Nos maisons modulaires sont à part. Tout d'abord elles sont écologiques, grâce à leurs matériaux écoresponsables.
+    					De plus la maison modulaire permet de créer la maison de tout les envies. </p>
+    			</td>
+    		</tr>
+			
+    	</table>
+
+<div>
 @endsection
