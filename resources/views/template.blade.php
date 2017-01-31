dcd9d1551d616e771e7ecbdf350b157a7257ef47
Julien Benoit <benoitjulien21560@gmail.com>
2017-01-31 09:13:32 +0100

    changement lien menu

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -31,7 +31,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/madera/public/home">Accueil</a>
+      <a class="navbar-brand" href="{!! url('home') !!}">Accueil</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
